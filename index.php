1eff2c395b9fe9fc12970bd289f20ed92eedf334
ALmax-git <alimustaphashettima@gmail.com>
2024-10-08 16:30:37 +0100

    Initial commit for Job_Application_Finder project

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Job Finder</title>
+        <link rel="stylesheet" href="asset/bootstrap/dist/css/bootstrap.min.css">
+    </head>
+
+    <body>
+        <div class="container">
+            <nav class="bg-info rounded p-auto">
+                <div class="row d-flex">
+                    <div class="col">
+                        <h1>Job Finder</h1>
+                    </div>
+                    <div class="col">
+                        <button class="btn btn-primary float-end">Profile</button>
+                    </div>
+
+                </div>
+            </nav>
+            <div class="content">
+            </div>
+        </div>
+        <script src="asset/bootstrap/dist/js/bootstrap.min.js"></script>
+    </body>
+
+</html>
